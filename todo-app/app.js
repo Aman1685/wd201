5228db1697e3354c994d95b6d2f0228dabe206fd
aman <aman@aman>
2024-09-13 18:20:03 +0530

    adding passport

--- todo-app/app.js
+++ todo-app/app.js
@@ -7,12 +7,55 @@ const bodyParser = require("body-parser");
 app.use(bodyParser.json());
 const path = require("path");
 const { title } = require("process");
+const { User } = require("./models");
+const { password } = require("pg/lib/defaults");
+const passport = require('passport');
+const connectEnsureLogin = require('connect-ensure-login');
+const session = require('express-session');
+const LocalStrategy = require('passport-local');
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser("shh! some secret string"));
 app.use(csrf({ cookie: true }));
 app.use(express.static(path.join(__dirname, 'public')));
-
 app.set("view engine", "ejs");
+app.use(session({
+  secret: "my-super-secret-key-10203040506070809",
+  cookie:{
+    maxAge: 24*60*60*100
+  }
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(new LocalStrategy({
+  usernameField: 'email',
+  passwordField: 'password'
+}, (username, password, done) => {
+  User.findOne({where: {email: username, password: password}})
+  .then((user) => {
+    return done(null, user)
+  }).catch((error) => {
+    return (error)
+  })
+}
+));
+
+passport.serializeUser((user, done) => {
+  console.log("Serializing user in session", user.id)
+  done(null, user.id)
+});
+
+passport.deserializeUser((id, done) => {
+  User.findByPk(id)
+  .then(user => {
+    done(null, user)
+  })
+  .catch(error => {
+    done(error, null)
+  })
+});
+
 
 app.put("/todos/:id", async function (request, response) {
   const todoId = request.params.id;
@@ -61,25 +104,50 @@ app.post("/todos", async function (request, response) {
   }
 });
 app.get('/', async (request, response) => {
-  const allTodos = await Todo.getTodos();
-  const overdue = await Todo.overdue();
-  const dueToday = await Todo.dueToday();
-  const dueLater = await Todo.dueLater();
-  if (request.accepts("html")) {
     response.render("index", {
       title: "Todo application",
-      overdue,
-      dueToday,
-      dueLater,
-      allTodos,
       csrfToken: request.csrfToken(),
-    })
-  } else {
-    response.json({
-      overdue,
-      dueToday,
-      dueLater,
-    })
-    } });
+    });
+    });
+  app.get("/signup", async (request, response) => {
+    response.render("signup", {title :"Signup", csrfToken: request.csrfToken()})
+  });
 
+  app.post("/users", async (request, response) => {
+    try {
+      console.log("Firstname", request.body.firstName);
+      const user = await User.create({
+        firstName: request.body.firstName,
+        lastName: request.body.lastName,
+        email: request.body.email,
+        password: request.body.password 
+    });
+    response.redirect("/");
+  } catch(error) {
+    console.log(error);
+  }
+  })
+
+  app.get("/todos",async (request, response) => {
+    const allTodos = await Todo.getTodos();
+    const overdue = await Todo.overdue();
+    const dueToday = await Todo.dueToday();
+    const dueLater = await Todo.dueLater();
+    if (request.accepts("html")) {
+      response.render("todos", {
+        title: "Todo application",
+        overdue,
+        dueToday,
+        dueLater,
+        allTodos,
+        csrfToken: request.csrfToken(),
+      })
+    } else {
+      response.json({
+        overdue,
+        dueToday,
+        dueLater,
+      }) 
+    } 
+    });
 module.exports = app;
